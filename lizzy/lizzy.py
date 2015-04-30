97ae171273decb7eaa23943b4da9106c1cb7416b
João Santos <joao.santos@zalando.de>
2015-04-30 15:10:23 +0200

    https://techjira.zalando.net/browse/CD-406 Added dummy job with locking

--- lizzy/lizzy.py
+++ lizzy/lizzy.py
@@ -3,12 +3,14 @@
 import logging
 
 import apscheduler.schedulers.background as scheduler_background
-import apscheduler.jobstores.redis as redis_jobstore
 import apscheduler.triggers.interval as scheduler_interval
 import connexion
 import flask
 import rod.connection
 
+import lizzy.models.deployment
+
+import time
 
 PORT = 9000
 
@@ -21,24 +23,30 @@ request = flask.request
 
 def check_status():
     logger = logging.getLogger('lizzy.job')
+    all_deployments = lizzy.models.deployment.Deployment.all()
     logger.debug('In Job')
+    for deployment in all_deployments:
+        if deployment.lock(5000):
+            logger.debug('ID: %s, STATUS: %s', deployment.deployment_id, deployment.status)
+        else:
+            logger.debug('Deployment object was locked skipping')
+
 
 # configure scheduler
-redis_jobstore.RedisJobStore()
 scheduler = scheduler_background.BackgroundScheduler()
-every_10_seconds = scheduler_interval.IntervalTrigger(seconds=10)
-scheduler.add_job(check_status, every_10_seconds)
+every_10_seconds = scheduler_interval.IntervalTrigger(seconds=5)
+scheduler.add_job(check_status, every_10_seconds, max_instances=10)
 
 # configure app
 swagger_app = connexion.App(__name__, PORT, specification_dir='swagger/')
 swagger_app.add_api('lizzy.v1.yaml')
 
 
-
 def main():
     # TODO make redis configurable
     root_logger.info('Connecting to Redis')
     rod.connection.setup(redis_host='localhost', port=6379)
+    root_logger.info('Connected to Redis')
 
     root_logger.info('Starting Scheduler')
     scheduler.start()
--- lizzy/models/deployment.py
+++ lizzy/models/deployment.py
@@ -1,23 +1,34 @@
 import uuid
-import rod.model as redis
+
+import rod.model
+import yaml
 
 
-class Deployment(redis.Model):
+class Deployment(rod.model.Model):
 
     prefix = 'lizzy_deployment'
     key = 'deployment_id'
     search_properties = ['deployment_id']
 
-    def __init__(self, deployment_id: str=None,
-                 *,
-                 deployment_strategy: str=None,
-                 image_version: str=None,
-                 senza_yaml: str=None):
+    def __init__(self, *,
+                 deployment_id: str=None,
+                 deployment_strategy: str,
+                 image_version: str,
+                 senza_yaml: str,
+                 stack_name: str=None,
+                 status: str='LIZZY_NEW',
+                 **kwargs):
         self.deployment_id = deployment_id if deployment_id is not None else str(uuid.uuid1())
-        # TODO if uid load else validate
-        self.image_version = image_version
         self.deployment_strategy = deployment_strategy
+        self.image_version = image_version
         self.senza_yaml = senza_yaml
+        if stack_name is None:
+            # TODO: error handling
+            senza_definition = yaml.safe_load(senza_yaml)
+            self.stack_name = senza_definition['SenzaInfo']['StackName']
+        else:
+            self.stack_name = stack_name
+        self.status = status  # status is cloud formation status or LIZZY_NEW
 
     def as_dict(self):
         return self.__dict__
