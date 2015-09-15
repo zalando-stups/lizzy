0daa7fdd3a3b1754232b26773f79d2adee014de3
João Santos <joao.santos@zalando.de>
2015-09-15 14:54:54 +0200

    #19 Json logger

--- lizzy/logging.py
+++ lizzy/logging.py
@@ -1,58 +1,53 @@
+from collections import OrderedDict
 import datetime
+import json
 import logging
 import traceback
 
 
-class KVPFormatter(logging.Formatter):
+# the following keys will be ignored unless specifically added
+DEFAULT_LOG_RECORD_KEYS = (
+    'filename', 'levelno', 'lineno', 'msecs', 'threadName', 'exc_text', 'msg', 'name', 'processName', 'thread',
+    'relativeCreated', 'process', 'exc_info', 'args', 'created', 'pathname', 'funcName', 'levelname', 'module',
+    'stack_info')
 
+
+class JsonFormatter(logging.Formatter):
     need_quoting = [' ', '/', '"', '\'']
 
-    def __escape_value(self, value):
-        if isinstance(value, str):
-            needs_to_be_escaped = any(char in value for char in self.need_quoting)
-            if needs_to_be_escaped:
-                if '\"' in value:
-                    value = repr(value)
-                else:
-                    # if value doesn't have double quotes just enclose it in double quotes
-                    value = '"{value}"'.format(value=value)
-        return value
-
     def format(self, record: logging.LogRecord):
-        values = vars(record)
+        record_values = vars(record)
 
-        # special cases
+        values = OrderedDict()
+        values['logger'] = record.name  # The name of the logger used to log the event represented
+        values['log_level'] = record.levelname  # Text logging level for the message
+        values['time'] = datetime.datetime.fromtimestamp(record.created).isoformat()
+        values['function'] = '{}.{}'.format(record.module, record.funcName)
+        values['line'] = record.lineno
         try:
             values['msg'] = str(record.msg) % record.args
-            del values['args']
-        except TypeError as e:
+        except Exception as e:  # pragma: no cover
             values['LOGGING_ERROR'] = str(e)
 
-        values['created'] = datetime.datetime.fromtimestamp(values['created'])
-
         # if there is an exception convert to string
-        if values['exc_info']:
-            exc_info = values['exc_info']
-            values['exc_info'] = exc_info and repr(exc_info)
+        if vars(record)['exc_info']:
+            exc_info = record_values['exc_info']
             values['traceback'] = ''.join(traceback.format_tb(exc_info[2]))
-            values['exception'] = exc_info[1]
-        else:
-            values['traceback'] = None
-            values['exception'] = None
+            values['exception'] = str(exc_info[1]).strip()
 
-        log_line = ' '.join('{key}={value}'.format(key=key, value=self.__escape_value(value))
-                            for key, value
-                            in values.items())
+        # extra
+        extra = {key: value for key, value in record_values.items() if key not in DEFAULT_LOG_RECORD_KEYS}
+        values.update(extra)
 
-        if values['traceback']:  # include traceback in human readable format
-            log_line += '\n{traceback}{exception}'.format_map(values)
+        log_line = json.dumps(values, default=repr)
+
         return log_line
 
 
 def init_logging():
     root_logger = logging.getLogger('')
     sh = logging.StreamHandler()
-    sh.setFormatter(KVPFormatter())
+    sh.setFormatter(JsonFormatter())
     root_logger.addHandler(sh)
     root_logger.setLevel(logging.DEBUG)
 
