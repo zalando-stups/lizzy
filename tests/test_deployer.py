74932cd8d7879c18093a75070b25c7680844f6bc
João Santos <joao.santos@zalando.de>
2015-09-16 11:11:12 +0200

    #18 test Deployer.deploying

--- tests/test_deployer.py
+++ tests/test_deployer.py
@@ -25,7 +25,9 @@ LIZZY_STACKS = {'stack-1': {'stack_id': 'stack-1',
                                    'status': 'LIZZY:REMOVED',
                                    'parameters': ['parameter1', 'parameter2']}}
 
-CF_STACKS = {'lizzy': {'42': {'status': 'TEST:STATE'}}}
+CF_STACKS = {'lizzy': {'42': {'status': 'TEST'},
+                       'inprog': {'status': 'CREATE_IN_PROGRESS'},
+                       'deployed': {'status': 'CREATE_COMPLETE'}}}
 
 
 @pytest.fixture
@@ -40,7 +42,7 @@ def test_log_info():
     stack = Stack(stack_id='lizzy-42', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7, image_version='1.0',
                   senza_yaml='senza:yaml', stack_name='lizzy', stack_version='42', parameters=[], status='LIZZY:TEST')
     deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
-    assert deployer.log_info == {'cf_status': 'TEST:STATE',
+    assert deployer.log_info == {'cf_status': 'TEST',
                                  'lizzy.stack.id': 'lizzy-42',
                                  'lizzy.stack.name': 'lizzy',
                                  'lizzy.stack.traffic': 7}
@@ -66,3 +68,37 @@ def test_new(monkeypatch, logger):
     mock_senza.create.return_value = False
     deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
     assert deployer.handle() == 'LIZZY:ERROR'
+
+
+def test_deploying(monkeypatch, logger):
+    mock_senza = MagicMock()
+    mock_senza.return_value = mock_senza
+    monkeypatch.setattr('lizzy.job.deployer.Senza', mock_senza)
+
+    stack = Stack(stack_id='nonexisting-42', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
+                  image_version='1.0', senza_yaml='senza:yaml', stack_name='nonexisting', stack_version='42',
+                  parameters=[], status='LIZZY:DEPLOYING')
+
+    deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
+    assert deployer.handle() == 'LIZZY:REMOVED'
+
+    stack = Stack(stack_id='lizzy-inprog', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
+                  image_version='1.0', senza_yaml='senza:yaml', stack_name='lizzy', stack_version='inprog',
+                  parameters=[], status='LIZZY:DEPLOYING')
+
+    deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
+    assert deployer.handle() == 'LIZZY:DEPLOYING'
+
+    stack = Stack(stack_id='lizzy-deployed', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
+                  image_version='1.0', senza_yaml='senza:yaml', stack_name='lizzy', stack_version='deployed',
+                  parameters=[], status='LIZZY:DEPLOYING')
+
+    deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
+    assert deployer.handle() == 'LIZZY:DEPLOYED'
+
+    stack = Stack(stack_id='lizzy-42', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
+                  image_version='1.0', senza_yaml='senza:yaml', stack_name='lizzy', stack_version='42',
+                  parameters=[], status='LIZZY:DEPLOYING')
+
+    deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
+    assert deployer.handle() == 'CF:TEST'
