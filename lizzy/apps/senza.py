7b56267a52817b89c4e061ed14cb2188c49320ba
João Santos <joao.santos@zalando.de>
2015-12-11 13:52:27 +0100

    #30 Documented senza wrapper

--- lizzy/apps/senza.py
+++ lizzy/apps/senza.py
@@ -1,3 +1,4 @@
+from typing import Optional, List, Dict
 import tempfile
 
 from .common import ExecutionError, Application
@@ -7,7 +8,16 @@ class Senza(Application):
     def __init__(self, region: str):
         super().__init__('senza', extra_parameters=['--region', region])
 
-    def create(self, senza_yaml: str, stack_version: str, image_version: str, parameters: list) -> bool:
+    def create(self, senza_yaml: str, stack_version: str, image_version: str, parameters: List[str]) -> bool:
+        """
+        Create a new stack
+
+        :param senza_yaml: Senza Definition
+        :param stack_version: New stack's version
+        :param image_version: Docker image to deployed
+        :param parameters: Extra parameters for the deployment
+        :return: Success of the operation
+        """
         with tempfile.NamedTemporaryFile() as temp_yaml:
             temp_yaml.write(senza_yaml.encode())
             temp_yaml.file.flush()
@@ -18,21 +28,35 @@ class Senza(Application):
                 self.logger.error('Failed to create stack.', extra={'command.output': e.output})
                 return False
 
-    def domains(self, stack_name: str=None):
+    def domains(self, stack_name: Optional[str]=None) -> List[Dict[str, str]]:
+        """
+        Get domain names for applications. If stack name is provided then it will show the domain names just for that
+        application
+
+        :param stack_name: Name of the application stack
+        :return: Route53 Domains
+        """
         if stack_name:
             stack_domains = self._execute('domains', stack_name, expect_json=True)
         else:
             stack_domains = self._execute('domains', expect_json=True)
         return stack_domains
 
-    def list(self) -> list:
+    def list(self) -> List[Dict]:
         """
-        Returns the stack list
+        Returns a list of all the stacks
         """
-        stacks = self._execute('list', expect_json=True)
+        stacks = self._execute('list', expect_json=True)  # type: list
         return stacks
 
     def remove(self, stack_name: str, stack_version: str) -> bool:
+        """
+        Removes a stack
+
+        :param stack_name: Name of the application stack
+        :param stack_version: Name of the application version that will be removed
+        :return: Success of the operation
+        """
         try:
             self._execute('delete', stack_name, stack_version)
             return True
@@ -40,6 +64,14 @@ class Senza(Application):
             self.logger.error('Failed to delete stack.', extra={'command.output': e.output})
             return False
 
-    def traffic(self, stack_name: str, stack_version: str, percentage: int):
+    def traffic(self, stack_name: str, stack_version: str, percentage: int) -> List[Dict]:
+        """
+        Changes the application traffic percentage.
+
+        :param stack_name: Name of the application stack
+        :param stack_version: Name of the application version that will be changed
+        :param percentage: New percentage
+        :return: Traffic weights for the application
+        """
         traffic_weights = self._execute('traffic', stack_name, stack_version, str(percentage), expect_json=True)
         return traffic_weights
