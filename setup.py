79c7bba0da0eab54e37249895da465aa9f302249
João Santos <joao.santos@zalando.de>
2015-12-14 14:02:41 +0100

    #30 Api level tests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(TestCommand):
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.cov = None
-        self.pytest_args = ['--cov', 'lizzy', '--cov-report', 'term-missing']
+        self.pytest_args = ['--cov', 'lizzy', '--cov-report', 'term-missing', '-v']
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
