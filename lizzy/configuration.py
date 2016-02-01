a89b0ccba1408e0952e69c8c072fc68675885fc8
João Santos <joao.santos@zalando.de>
2016-02-01 14:00:18 +0100

    #30 Improve documentation

--- lizzy/configuration.py
+++ lizzy/configuration.py
@@ -1,20 +1,10 @@
-"""
-Copyright 2015 Zalando SE
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
-License. You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
- language governing permissions and limitations under the License.
-"""
-
 import environmental
 
 
 class Configuration:
+    """
+    Configuration parameters to be fetched from the environment
+    """
     allowed_users = environmental.List('ALLOWED_USERS', None)
     deployer_scope = environmental.Str('DEPLOYER_SCOPE')  # OAUTH scope needed to deploy
     job_interval = environmental.Int('JOB_INTERVAL', 15)  # how many seconds to wait between job runs
