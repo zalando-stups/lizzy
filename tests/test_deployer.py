de3e2f18a8211211ced7f334089aa7e97ba4d638
João Santos <joao.santos@zalando.de>
2016-03-30 11:13:04 +0200

    #82 Fix unit tests

--- tests/test_deployer.py
+++ tests/test_deployer.py
@@ -126,7 +126,7 @@ def test_deploying_create_in_progress(monkeypatch, logger):
                   status='CF:CREATE_IN_PROGRESS')
 
     deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
-    assert deployer.handle() == 'LIZZY:REMOVED'
+    assert deployer.handle() is REMOVED_STACK
 
     stack = Stack(stack_id='lizzy-inprog', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
                   image_version='1.0', senza_yaml=YAML1, stack_name='lizzy', stack_version='inprog',
