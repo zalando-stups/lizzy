5e31effe7fa636f9b776fde18991603f21c22513
João Santos <joao.santos@zalando.de>
2016-04-26 15:41:40 +0200

    #108 Update unit tests

--- tests/test_deployer.py
+++ tests/test_deployer.py
@@ -112,7 +112,7 @@ def test_deploying(monkeypatch, logger):
                   status='LIZZY:DEPLOYING')
 
     deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
-    assert deployer.handle() == 'LIZZY:DEPLOYED'
+    assert deployer.handle() == 'CF:CREATE_COMPLETE'
 
     stack = Stack(stack_id='lizzy-42', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
                   image_version='1.0', senza_yaml=YAML1, stack_name='lizzy', stack_version='42',
@@ -146,7 +146,7 @@ def test_deploying_create_in_progress(monkeypatch, logger):
                   status='CF:CREATE_IN_PROGRESS')
 
     deployer = Deployer('region', LIZZY_STACKS, CF_STACKS, stack)
-    assert deployer.handle() == 'LIZZY:DEPLOYED'
+    assert deployer.handle() == 'CF:CREATE_COMPLETE'
 
     stack = Stack(stack_id='lizzy-42', creation_time='2015-09-16T09:48', keep_stacks=2, traffic=7,
                   image_version='1.0', senza_yaml=YAML1, stack_name='lizzy', stack_version='42',
